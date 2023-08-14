b91397dd81f63bbd9da01651bc66ace9d33901ea
Kivi Shapiro <github.com@kivis.net>
2023-08-14 17:33:05 -0400

    Make the review page show the actual chosen values

--- NCLL/catch-and-throw.js
+++ NCLL/catch-and-throw.js
@@ -1,11 +1,10 @@
 const queryString = window.location.search
 const urlParams = new URLSearchParams ( queryString )
 
-for ( var parmName of urlParams ) {
-    console.log ( parmName, urlParams [ parmName ] )
+for ( const [ key, value ] of urlParams ) {
     
   // Remove existing namespace prefix
-    let fieldName = parmName.replace ( /^CAT_/, '' )
+    let fieldName = key.replace ( /^CAT_/, '' )
 
   // Add new namespace prefix
     fieldName = "CAT_" + fieldName
@@ -14,7 +13,7 @@ for ( var parmName of urlParams ) {
     let inputField = document.createElement ( "input" )
     inputField.setAttribute( "type", "hidden" )
     inputField.setAttribute( "name", fieldName )
-    inputField.setAttribute( "value", urlParams [ parmName ] )
+    inputField.setAttribute( "value", value )
 
   // Append to form
     document .forms [0] .appendChild ( inputField )
