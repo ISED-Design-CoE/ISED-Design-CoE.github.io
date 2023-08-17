9e0496b10678499fe5a6192a12a650109f599027
Kivi Shapiro <github.com@kivis.net>
2023-08-16 23:29:46 -0400

    Llicence fee calculation

--- NCLL/catch-and-throw.js
+++ NCLL/catch-and-throw.js
@@ -1,14 +1,18 @@
+function createHiddenField ( fieldName, fieldValue ) {
+  let inputField = document.createElement ( "input" )
+  inputField.setAttribute( "type", "hidden" )
+  inputField.setAttribute( "name", fieldName )
+  inputField.setAttribute( "value", fieldValue )
+
+  // Append to form
+  document .forms [0] .appendChild ( inputField )
+}
+
 const queryString = window.location.search
 const urlParams = new URLSearchParams ( queryString )
 
 for ( const [ key, value ] of urlParams ) {
     
   // Create hidden input field (which will be thrown forward to the next screen because GET)
-    let inputField = document.createElement ( "input" )
-    inputField.setAttribute( "type", "hidden" )
-    inputField.setAttribute( "name", key )
-    inputField.setAttribute( "value", value )
-
-  // Append to form
-    document .forms [0] .appendChild ( inputField )
+  createHiddenField ( key, value )
 }
