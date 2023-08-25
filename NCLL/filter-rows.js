fd1bdf3ffe6371d57c14d8ce5629a1187b93689f
Jacob Fodale <jacob.fodale@ised-isde.gc.ca>
2023-08-25 12:32:38 -0400

    Functional version (does not follow guidelines)
    This commit includes the js but is not pleasant to look at.

--- NCLL/filter-rows.js
+++ NCLL/filter-rows.js
@@ -1,11 +1,41 @@
+var currentStatus = []
+
+function filterTable() {
+	status_set = new Set(currentStatus)
+	const rows = document.querySelectorAll('.data_row');
+	rows.forEach(row => {
+		if (currentStatus.length == 0) {
+			row.removeAttribute('hidden')
+		} else {
+			match = false;
+			row.className.split(' ').forEach(row_status => {
+				if (status_set.has(row_status)){
+					row.removeAttribute('hidden')
+					match = true
+				}
+				
+			})
+			if (!match) {
+				row.setAttribute('hidden','hidden')
+			}
+		}
+	})
+}
+
 const urlParams = new URLSearchParams (window.location.search)
 const filter = urlParams.get("filter");
 if (filter != null) {
-	console.log(filter);
-	const rows = document.querySelectorAll('.data_row');
-	rows.forEach(row => {
-	if (!row.classList.contains(filter)) {
-		row.style.display = "none";
+	currentStatus.push(filter)
+	filterTable()
+	document.getElementById(filter+"_checkbox").setAttribute('checked','checked')
+}
+
+function toggleStatus(status) {
+	if (currentStatus.indexOf(status) === -1 ) {
+		currentStatus.push(status)
+	} else {
+		currentStatus.splice(currentStatus.indexOf(status),1)
 	}
-				})
-			}
+	filterTable()
+	
+}
