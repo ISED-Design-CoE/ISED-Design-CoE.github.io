142b2b6791bd7ca9e801ee3c5e30462f0290ac12
Jacob Fodale <jacob.fodale@ised-isde.gc.ca>
2023-11-06 16:19:16 -0500

    show scale, area and message. Fix buttons

--- WKT-Tool-GMaps/maps.js
+++ WKT-Tool-GMaps/maps.js
@@ -1,26 +1,40 @@
 // TODOs:
 // - Google's search
-// - Show Area
-//    - Show message based on area
+// - Show Area X
+//    - Show message based on area X
 // - Fix button location
-// - show scale
+// - show scale X
 // - remove street view
+// errors when more than 13 points
+
+let polygon;
+
+let ignore_drawing = false;
+
+let warnings = {
+  medium: "<p>The area is between 75 km<sup>2</sup> and 165 km<sup>2</sup>. It can only be used with <b>medium power</b>.</sup></p>",
+  low: "<p>The area is between 0 km<sup>2</sup> and 15 km<sup>2</sup>. It can only be used with <b>low power</b>.</sup></p>",
+  large: "<p>The area is too large. Please reduce the size of the polygon to be less than 165km<sup>2</sup></p>",
+  between: "<p>The area is between 15 km<sup>2</sup> and 75 km<sup>2</sup>. Please reduce or increase your area size beyond these limits.</p>",
+  morethantwelve: "<p>The "
+}
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 63.272, lng: -100.581 },
     zoom: 4,
+    scaleControl: true,
   });
   const drawingManager = new google.maps.drawing.DrawingManager({
     drawingMode: null,
     drawingControl: false,
     polygonOptions: {
       editable: true,
-      fillColor: '#bf00ff',
+      fillColor: '#33cc33',
       fillOpacity: 0.65
     },
     drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
+      position: google.maps.ControlPosition.LEFT_TOP,
       drawingModes: [
         google.maps.drawing.OverlayType.POLYGON,
 
@@ -33,27 +47,41 @@ function initMap() {
   var removeControlDiv = document.createElement('div');
   var removeControl = new RemoveControl(removeControlDiv);
 
+  var drawactiveControlDiv = document.createElement('div');
+  var drawactiveControl = new DrawActiveControl(drawactiveControlDiv);
+
   var drawControlDiv = document.createElement('div');
   var drawControl = new DrawControl(drawControlDiv);
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(drawControlDiv);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(drawControlDiv);
+
 
   var areaControlDiv = document.createElement('div');
   var areaControl = new AreaControl(areaControlDiv);
-  map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(areaControlDiv);
-
-
-  var poly;
+  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(areaControlDiv);
 
   // ON DRAW
 
-  google.maps.event.addListener(drawingManager, "polygoncomplete", function (polygon) {
-    poly = polygon
+  google.maps.event.addListener(drawingManager, "polygoncomplete", function (event) {
+
+    polygon = event
+    if (ignore_drawing) {
+      ignore_drawing = false;
+      polygon.setMap(null);
+      return;
+    }
+
+    while (polygon.getPath().getLength() > 12) {
+      polygon.getPath().pop();
+    }
+
     google.maps.event.addListener(polygon, 'click', function (mev) {
       if (mev.vertex != null) {
         polygon.getPath().removeAt(mev.vertex);
       }
     });
 
+
+    // WKT
     let wkt = "POLYGON(("
     for (const elem of polygon.getPath().getArray()) {
       wkt += elem.lng().toFixed(6) + " "
@@ -62,22 +90,91 @@ function initMap() {
     wkt = wkt.substring(0, wkt.length - 1)
     wkt += "))"
     console.log(wkt)
+
+
     drawingManager.setOptions({
       drawingMode: null,
       drawingControl: false,
     });
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(removeControlDiv);
+    map.controls[google.maps.ControlPosition.LEFT_TOP].pop()
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(removeControlDiv);
     document.getElementById("WKT").value = wkt;
-    document.getElementById ( "wkt-output" ).removeAttribute("hidden");
 
-    document.getElementById("area").innerHTML = "<b>Area Size</b>: "+ ((google.maps.geometry.spherical.computeArea(polygon.getPath())/1000000).toFixed(3)) + " km<sup>2</sup>"
+    let area = (google.maps.geometry.spherical.computeArea(polygon.getPath()) / 1000000).toFixed(3)
+
+    document.getElementById("area").innerHTML = "<b>Area size</b>: " + area + " km<sup>2</sup>"
+
+    document.getElementById("size-alert").removeAttribute('hidden')
+    if (area < 15) {
+      document.getElementById("alert-text").innerHTML = warnings.low
+      document.getElementById("size-alert").classList = "alert alert-info"
+      document.getElementById("wkt-output").removeAttribute("hidden")
+    } else if (area >= 15 && area <= 75) {
+      document.getElementById("alert-text").innerHTML = warnings.between
+      document.getElementById("size-alert").classList = "alert alert-danger"
+      polygon.setOptions({
+        fillColor: '#760000',
+        fillOpacity: 0.65
+      })
+    } else if (area < 165) {
+      document.getElementById("alert-text").innerHTML = warnings.medium
+      document.getElementById("size-alert").classList = "alert alert-info"
+      document.getElementById("wkt-output").removeAttribute("hidden")
+    } else {
+      document.getElementById("alert-text").innerHTML = warnings.large
+      document.getElementById("size-alert").classList = "alert alert-danger"
+      polygon.setOptions({
+        fillColor: '#760000',
+        fillOpacity: 0.65
+      })
+    }
+
+
+
+    checkVertexAmount()
   });
 
+  function checkVertexAmount() {
+    google.maps.event.addListener(polygon.getPath(), "insert_at", function (index) {
+      console.log('test')
+      if (polygon.getPath().getLength() > 12) {
+        polygon.getPath().removeAt(index)
+      }
+    })
+  };
 
 
 
+  // BUTTONS
 
-// BUTTONS
+  function DrawActiveControl(controlDiv) {
+    var controlUI = document.createElement('div');
+    controlUI.title = 'Remove polygon';
+
+    controlUI.style.backgroundImage = 'url(images/draw.svg)'
+    controlUI.style.backgroundColor = '#EBEBEB';
+    controlUI.style.backgroundPosition = '50%';
+    controlUI.style.width = '6em'
+    controlUI.style.height = '6em'
+    controlUI.style.backgroundSize = '80%'
+    controlUI.style.backgroundRepeat = "no-repeat";
+    controlUI.style.borderRadius = "2px";
+    controlUI.style.border = "1px solid rgba(0,0,0,0.2)"
+    controlUI.style.margin = "10px"
+    controlUI.style.boxShadow = "rgba(0, 0, 0, 0.3) 0px 1px 4px -1px"
+    controlDiv.appendChild(controlUI);
+
+    google.maps.event.addDomListener(controlUI, 'click', function () {
+      ignore_drawing = true
+      drawingManager.setOptions({ drawingMode: null })
+      map.controls[google.maps.ControlPosition.LEFT_TOP].pop()
+      map.controls[google.maps.ControlPosition.LEFT_TOP].push(drawControlDiv);
+      document.getElementById("wkt-output").setAttribute("hidden", 'true');
+      
+
+    });
+
+  }
 
   function RemoveControl(controlDiv) {
     var controlUI = document.createElement('div');
@@ -89,17 +186,18 @@ function initMap() {
     controlUI.style.width = '6em'
     controlUI.style.height = '6em'
     controlUI.style.backgroundSize = '95%'
-    controlUI.style.backgroundRepeat= "no-repeat";
-    controlUI.style.borderRadius= "2px";
-    controlUI.style.border= "3px solid rgba(0,0,0,0.2)"
+    controlUI.style.backgroundRepeat = "no-repeat";
+    controlUI.style.borderRadius = "2px";
+    controlUI.style.border = "1px solid rgba(0,0,0,0.2)"
     controlUI.style.margin = "10px"
+    controlUI.style.boxShadow = "rgba(0, 0, 0, 0.3) 0px 1px 4px -1px"
     controlDiv.appendChild(controlUI);
 
     google.maps.event.addDomListener(controlUI, 'click', function () {
-      poly.setMap(null);
-      map.controls[google.maps.ControlPosition.TOP_CENTER].pop()
-      map.controls[google.maps.ControlPosition.TOP_CENTER].push(drawControlDiv);
-      document.getElementById ( "wkt-output" ).setAttribute("hidden",'true');
+      polygon.setMap(null);
+      map.controls[google.maps.ControlPosition.LEFT_TOP].pop()
+      map.controls[google.maps.ControlPosition.LEFT_TOP].push(drawControlDiv);
+      document.getElementById("wkt-output").setAttribute("hidden", 'true');
 
     });
 
@@ -115,16 +213,18 @@ function initMap() {
     controlUI.style.width = '6em'
     controlUI.style.height = '6em'
     controlUI.style.backgroundSize = '80%'
-    controlUI.style.backgroundRepeat= "no-repeat";
-    controlUI.style.borderRadius= "2px";
-    controlUI.style.border= "3px solid rgba(0,0,0,0.2)"
+    controlUI.style.backgroundRepeat = "no-repeat";
+    controlUI.style.borderRadius = "2px";
+    controlUI.style.border = "1px solid rgba(0,0,0,0.2)"
     controlUI.style.margin = "10px"
-    controlUI.style.boxShadow = "0 0 0 2pt black;"
+    controlUI.style.boxShadow = "rgba(0, 0, 0, 0.3) 0px 1px 4px -1px"
+    controlUI.style.cursor = "pointer"
     controlDiv.appendChild(controlUI);
 
     google.maps.event.addDomListener(controlUI, 'click', function () {
-      drawingManager.setOptions({drawingMode:google.maps.drawing.OverlayType.POLYGON})
-      map.controls[google.maps.ControlPosition.TOP_CENTER].pop()
+      drawingManager.setOptions({ drawingMode: google.maps.drawing.OverlayType.POLYGON })
+      map.controls[google.maps.ControlPosition.LEFT_TOP].pop()
+      map.controls[google.maps.ControlPosition.LEFT_TOP].push(drawactiveControlDiv);
     });
 
   }
@@ -136,12 +236,12 @@ function initMap() {
     controlUI.title = 'Area size';
 
     controlUI.style.backgroundColor = 'white';
-    controlUI.style.borderRadius= "4px";
-    controlUI.style.border= "2px solid rgba(0,0,0,0.2)"
+    controlUI.style.borderRadius = "4px";
+    controlUI.style.border = "2px solid rgba(0,0,0,0.2)"
     controlUI.style.margin = "10px"
     controlUI.style.padding = "8px"
     controlUI.id = "area"
-    controlUI.innerHTML = '<b>Area Size: </b>'
+    controlUI.innerHTML = '<b>Area size: </b>'
 
     controlDiv.appendChild(controlUI);
 
