b74bb4abed05f626961e5985cf02cce957b7e089
Fodale <Jacob.Fodale@ised-isde.gc.ca>
2025-04-23 14:16:14 -0400

    add sawi documents

---
+++ SAWI/filter-rows.js
@@ -0,0 +1,43 @@
+var currentStatus = [];
+
+function filterTable() {
+  status_set = new Set(currentStatus);
+  const rows = document.querySelectorAll(".data_row");
+  rows.forEach((row) => {
+    if (currentStatus.length == 0) {
+      row.removeAttribute("hidden");
+    } else {
+      match = false;
+      if (status_set.has(row.getAttribute("type"))) {
+        row.removeAttribute("hidden");
+        match = true;
+      }
+      if (!match) {
+        row.setAttribute("hidden", "hidden");
+      }
+    }
+  });
+}
+
+$(document).on("wb-ready.wb", function (event) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const filter = urlParams.get("filter");
+  if (filter != null) {
+    currentStatus.push(filter);
+    filterTable();
+    console.log(filter + "_checkbox");
+    console.log(document.getElementById(filter + "_checkbox"));
+    document
+      .getElementById(filter + "_checkbox")
+      .setAttribute("checked", "checked");
+  }
+});
+
+function toggleStatus(status) {
+  if (currentStatus.indexOf(status) === -1) {
+    currentStatus.push(status);
+  } else {
+    currentStatus.splice(currentStatus.indexOf(status), 1);
+  }
+  console.log(currentStatus);
+}
