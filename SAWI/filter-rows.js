5f777243066a2cc37d9aae0ae43174af2597d764
Fodale <Jacob.Fodale@ised-isde.gc.ca>
2025-04-23 15:09:16 -0400

    add filter per auction

--- SAWI/filter-rows.js
+++ SAWI/filter-rows.js
@@ -1,16 +1,30 @@
-var currentStatus = [];
+var filters = { doc_type: [], auction: [] };
 
 function filterTable() {
-  status_set = new Set(currentStatus);
   const rows = document.querySelectorAll(".data_row");
   rows.forEach((row) => {
-    if (currentStatus.length == 0) {
+    if (filters["doc_type"].length == 0 && filters["auction"].length == 0) {
       row.removeAttribute("hidden");
     } else {
       match = false;
-      if (status_set.has(row.getAttribute("type"))) {
-        row.removeAttribute("hidden");
-        match = true;
+      if (filters["doc_type"].length != 0 && filters["auction"].length != 0) {
+        if (
+          filters["doc_type"].includes(row.getAttribute("doc_type")) &&
+          filters["auction"].includes(row.getAttribute("auction"))
+        ) {
+          row.removeAttribute("hidden");
+          match = true;
+        }
+      } else if (filters["doc_type"].length != 0) {
+        if (filters["doc_type"].includes(row.getAttribute("doc_type"))) {
+          row.removeAttribute("hidden");
+          match = true;
+        }
+      } else {
+        if (filters["auction"].includes(row.getAttribute("auction"))) {
+          row.removeAttribute("hidden");
+          match = true;
+        }
       }
       if (!match) {
         row.setAttribute("hidden", "hidden");
@@ -19,25 +33,27 @@ function filterTable() {
   });
 }
 
-$(document).on("wb-ready.wb", function (event) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const filter = urlParams.get("filter");
-  if (filter != null) {
-    currentStatus.push(filter);
-    filterTable();
-    console.log(filter + "_checkbox");
-    console.log(document.getElementById(filter + "_checkbox"));
-    document
-      .getElementById(filter + "_checkbox")
-      .setAttribute("checked", "checked");
-  }
-});
-
-function toggleStatus(status) {
-  if (currentStatus.indexOf(status) === -1) {
-    currentStatus.push(status);
+function toggleFilter(filter, type) {
+  if (filters[type].indexOf(filter) === -1) {
+    filters[type].push(filter);
   } else {
-    currentStatus.splice(currentStatus.indexOf(status), 1);
+    filters[type].splice(filters[type].indexOf(filter), 1);
   }
-  console.log(currentStatus);
+  console.log(filters);
 }
+
+// Could be used to filter tables in auction. Currently unused.
+
+// $(document).on("wb-ready.wb", function (event) {
+//   const urlParams = new URLSearchParams(window.location.search);
+//   const filter = urlParams.get("filter");
+//   if (filter != null) {
+//     currentStatus.push(filter);
+//     filterTable();
+//     console.log(filter + "_checkbox");
+//     console.log(document.getElementById(filter + "_checkbox"));
+//     document
+//       .getElementById(filter + "_checkbox")
+//       .setAttribute("checked", "checked");
+//   }
+// });
