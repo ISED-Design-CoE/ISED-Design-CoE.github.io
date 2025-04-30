09422ab9a55b3efb31199dcedd52b21a46daa179
Jacob Fodale <jacob.fodale@ised-isde.gc.ca>
2025-04-30 13:25:47 -0400

    make things 'prettier'

--- SAWI/populate-whats-new.js
+++ SAWI/populate-whats-new.js
@@ -1,29 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('filtered_output.json').then((res) => {
-        if (!res.ok) throw new Error("Failed to fetch filtered_output.json");
-        return res.json();
+  fetch("filtered_output.json")
+    .then((res) => {
+      if (!res.ok) throw new Error("Failed to fetch filtered_output.json");
+      return res.json();
     })
-        .then((data) => {
-            const whats_new_dl = document.getElementById("whats-new-dl");
-            if (!whats_new_dl) return console.error("Missing whats-new-dl");
+    .then((data) => {
+      const whats_new_dl = document.getElementById("whats-new-dl");
+      if (!whats_new_dl) return console.error("Missing whats-new-dl");
 
-            for (const [i, row] of data.entries()) {
-                if (i == 6) { break };
-                const dt = document.createElement("dt");
-                dt.classList = "label label-warning";
-                const span = document.createElement("span");
-                span.innerHTML = "Updated on " + row.Date;
-                dt.appendChild(span);
-                whats_new_dl.appendChild(dt);
-                const dd = document.createElement("dd");
-                dd.innerHTML = row.Title;
-                whats_new_dl.appendChild(dd);
-            }
-        }).catch((err) => {
-            const tbody = document.querySelector("#document-table tbody");
-            if (tbody)
-                tbody.innerHTML = `<tr><td colspan="4">Error loading data</td></tr>`;
-            console.error(err);
-        });
-
+      for (const [i, row] of data.entries()) {
+        if (i == 6) {
+          break;
+        }
+        const dt = document.createElement("dt");
+        dt.classList = "label label-warning";
+        const span = document.createElement("span");
+        span.innerHTML = "Updated on " + row.Date;
+        dt.appendChild(span);
+        whats_new_dl.appendChild(dt);
+        const dd = document.createElement("dd");
+        dd.innerHTML = row.Title;
+        whats_new_dl.appendChild(dd);
+      }
+    })
+    .catch((err) => {
+      const tbody = document.querySelector("#document-table tbody");
+      if (tbody)
+        tbody.innerHTML = `<tr><td colspan="4">Error loading data</td></tr>`;
+      console.error(err);
+    });
 });
