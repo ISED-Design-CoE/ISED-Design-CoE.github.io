6bfb4a405f4e27f1a22015a3cd9f1be96cb0ac76
Jacob Fodale <jacob.fodale@ised-isde.gc.ca>
2025-07-10 11:42:18 -0400

    Broadcast renewal | Add custom page to edit contact, review and success page (#110)
    * create a preview on the first page, and dedicate a full page to edit contact
    
    * reset when record is changed
    
    * add review and success, fix going from contact edit to main form
    
    * remove table

--- broadcast-licence-renewal/scripts/populate-inputs.js
+++ broadcast-licence-renewal/scripts/populate-inputs.js
@@ -9,24 +9,42 @@ function populate_input(record_id) {
       record = data[record_id];
 
       for (const [key, value] of Object.entries(record)) {
-        try {
+        if (document.getElementById(key) != null) {
+          document.getElementById(key).innerHTML = value;
+        }
+        if (document.getElementsByName(key)[0] != null) {
           document.getElementsByName(key)[0].value = value;
-        } catch (err) {
-          console.log(err);
         }
       }
-
-      document.getElementById("main-form").removeAttribute("hidden");
+      if (document.getElementById("contact-info") != null) {
+        document.getElementById("contact-info").removeAttribute("hidden");
+      }
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementById("record-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault(); // Stop the page from reloading
-      const formData = new FormData(this); // 'this' refers to the form element
-      console.log(document.getElementsByName("record")[0].value);
-      populate_input(document.getElementsByName("record")[0].value);
-    });
+  if (document.getElementById("record-form") != null) {
+    document
+      .getElementById("record-form")
+      .addEventListener("submit", function (event) {
+        event.preventDefault(); // Stop the page from reloading
+        const formData = new FormData(this); // 'this' refers to the form element
+        console.log(document.getElementsByName("record")[0].value);
+        populate_input(document.getElementsByName("record")[0].value);
+        if (document.getElementById("main-form") != null) {
+          document.getElementById("main-form").setAttribute("hidden", true);
+        }
+      });
+  }
+
+  if (document.getElementById("select-record") != null) {
+    console.log("test!");
+    document
+      .getElementById("select-record")
+      .addEventListener("gcdsChange", (event) => {
+        console.log("changed??");
+        document.getElementById("contact-info").setAttribute("hidden", true);
+        document.getElementById("main-form").setAttribute("hidden", true);
+      });
+  }
 });
