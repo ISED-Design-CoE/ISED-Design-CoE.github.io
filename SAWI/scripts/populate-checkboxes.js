f19fe67d602f4c3abd23df1463548ec65651b5bd
Jacob Fodale <jacob.fodale@ised-isde.gc.ca>
2025-07-14 15:17:07 -0400

    change filter, bands and more

--- SAWI/scripts/populate-checkboxes.js
+++ SAWI/scripts/populate-checkboxes.js
@@ -36,37 +36,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
   type_mapping = {
     en: {
-      Applicants: "Applicants",
-      "Band plan": "Band plan",
-      Comments: "Comments",
-      Consultation: "Consultation",
-      "Decision / Framework": "Framework",
-      "Decision / Framework / Outlook": "Outlook",
-      FAQ: "FAQ",
-      "Landing page": "Landing page",
-      Manual: "Manual",
-      Map: "Map",
-      Notice: "Notice",
-      Results: "Results",
-      "Spectrum Advisory Bulletins (SAB)": "Spectrum advisory bulletins ",
-      "Table of key dates": "Table of key dates",
+      Applicants: "Applicants (4)",
+      "Band plan": "Band plan (1)",
+      Comments: "Comments (11)",
+      Consultation: "Consultation (12)",
+      "Decision / Framework": "Framework (16)",
+      "Decision / Framework / Outlook": "Outlook (2)",
+      FAQ: "FAQ (5)",
+      "Landing page": "Landing page (1)",
+      Manual: "Manual (3)",
+      Map: "Map (2)",
+      Notice: "Notice (2)",
+      Results: "Results (15)",
+      "Spectrum Advisory Bulletins (SAB)": "Spectrum advisory bulletins (1)",
+      "Table of key dates": "Table of key dates (3)",
     },
     fr: {
-      Applicants: "Requérants",
-      "Band plan": "Plan de répartition des bandes",
-      Comments: "Commentaires",
-      Consultation: "Consultation",
-      "Decision / Framework": "Cadre de délivrance",
-      "Decision / Framework / Outlook": "Perspectives",
-      FAQ: "FAQ",
-      "Landing page": "Page d'accueil",
-      Manual: "Manuel",
-      Map: "Carte",
-      Notice: "Avis",
-      Results: "Résultats",
+      Applicants: "Requérants (4)",
+      "Band plan": "Plan de répartition des bandes (1)",
+      Comments: "Commentaires (11)",
+      Consultation: "Consultation (12)",
+      "Decision / Framework": "Cadre de délivrance (16)",
+      "Decision / Framework / Outlook": "Perspectives (2)",
+      FAQ: "FAQ (5)",
+      "Landing page": "Page d'accueil (1)",
+      Manual: "Manuel (3)",
+      Map: "Carte (3)",
+      Notice: "Avis (2)",
+      Results: "Résultats (15)",
       "Spectrum Advisory Bulletins (SAB)":
-        "Bulletins consultatifs sur le spectre",
-      "Table of key dates": "Tableau des dates clés",
+        "Bulletins consultatifs sur le spectre (1)",
+      "Table of key dates": "Tableau des dates clés (3)",
     },
   };
   function loadCheckboxes(url, containerId, filterType) {
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
               <label for="${id}">
                 <input type="checkbox" id="${id}"
                   onchange="toggleFilter('${type}','${filterType}')" />
-                ${mapped_type}
+                ${mapped_type} 
               </label>
             </li>`;
           })
