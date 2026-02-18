1673e06e35c303d89f8dc0c67625442af147c7cd
Rishi Singla <121279918+RishiSingla04@users.noreply.github.com>
2026-02-18 12:59:04 -0500

    Fix formatting condition for displaying format information

--- Site_Data_Upload_Help_Page/Terrestrial/Scripts/load_table.js
+++ Site_Data_Upload_Help_Page/Terrestrial/Scripts/load_table.js
@@ -102,8 +102,8 @@
           <h2>${fieldName}</h2>
           <p>${descHTML}</p>
           ${requiredWhen ? "<h4>Required when</h4><p>" + requiredWhen + "</p>" : ""}
-          ${formatRaw && formatRaw.trim() ? "<h4>Format</h4><p>" + formatRaw + "</p>" : ""}
           ${example && example.trim() ? "<h4>Example</h4><p>" + example + "</p>" : ""}
+          ${formatRaw && formatRaw.trim() ? "<h4>Format</h4><p>" + formatRaw + "</p>" : ""}
           ${errorMsg && errorMsg.trim() ? "<h4>Potential error(s)</h4><p><code style='display: inline-block; padding: 3px; white-space: pre-wrap; word-wrap'>" + errorMsg + "</code></p>" : ""}
           ${note && note.trim() ? note + "</p>" : ""}
         </td>
